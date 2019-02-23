44e6bdbf516e75824c0be2a5f811e30176a2e23f
Mr.Z <zrd0921@qq.com>
2019-02-23 16:17:35 +0800

    update

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,6 +3,10 @@ let source = `
 <template>
 	<vc-select />
 	<vcm-select />
+	<vc-select>
+		<vc-option-group>22</vc-option-group>
+		<vc-option>22</vc-option>
+	</vc-select>
 </template>
 <script>
 export default {
